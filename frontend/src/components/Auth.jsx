985afa7a92ff1b8966e97cb301942fd4150d0b77
sahadatt <ansarisahadat276@gmail.com>
2026-07-12 18:12:49 +0530

    Fixed backend URL

--- frontend/src/components/Auth.jsx
+++ frontend/src/components/Auth.jsx
@@ -15,7 +15,7 @@ const Auth = ({ onLoginSuccess }) => {
         const endpoint = isLogin ? '/api/auth/login' : '/api/auth/signup';
         
         try {
-            const response = await fetch(`http://localhost:5000${endpoint}`, {
+            const response = await fetch(`https://smartqueue-oi3s.onrender.com${endpoint}`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password })
